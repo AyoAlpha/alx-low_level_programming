fc797c72f3bb2b24191179691fd0d5a0b58bbc65
Ayodeji Adefeso <ayodejiadefeso@gmail.com>
2023-02-22 19:51:06 -0800

    PUTCHAR

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * main -Entry point
+ *
+ * Result: Always 0 (success)
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
+

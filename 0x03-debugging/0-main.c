4794aed139303b11844724ef0566bb55567080ac
Ayodeji Adefeso <ayodejiadefeso@gmail.com>
2023-02-23 15:38:31 -0800

    Task 2

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,75 @@
+nclude <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
 #include "main.h"
-
 /**
- * main -Test function for positive or negative
- * Return: 0
- */
-
+ *  * mqin -Test function for positive or negative
+ *   *Reurn: 0
+ *    */
 int main(void)
 {
-	int i;
-	
-	i = 0;
-	positive_or_negative(i);
+	        int i;
+#include "main.h"
+		/**
+		 *  * mqin -Test function for positive or negative
+		 *   *Reurn: 0
+		 *    */
+		int main(void)
+#include "main.h"
+			/**
+			 *  * mqin -Test function for positive or negative
+			 *   *Reurn: 0
+			 *    */
+			int main(void)
+			{
+#include "main.h"
+				/**
+				 *  * mqin -Test function for positive or negative
+				 *   *Reurn: 0
+				 *    */
+#include "main.h"
+				/**
+				 *  * mqin -Test function for positive or negative
+				 *   *Reurn: 0
+				 *    */
+#include "main.h"
+				/**
+				 *  * mqin -Test function for positive or negative
+				 *   *Reurn: 0
+				 *   #include "main.h"
+				 */**
+				 * mqin -Test function for positive or negative
+					 * #include "main.h"
+					 */**
+#include "main.h"
+					 /**
+					  *  * mqin -Test function for positive or negative
+					  *   *Reurn: 0
+					  *    */
+					 int main(void)
+					 {
+#include "main.h"
+						 /**
+						  *  * mqin -Test function for positive or negative
+						  *   *Reurn: 0
+						  *    */
+#include "main.h"
+						 /**
+						  *  * mqin -Test function for positive or negative
+						  *   *Reurn: 0
+						  *    */
+#include "main.h"
+						 /**
+						  *  * mqin -Test function for positive or negative
+						  *   *Reurn: 0
+						  *   #include "main.h"
+						  */**
+						  * mqin -Test function for positive or negative
+							  * #include "main.h"
+							  */**
+#include "main.h"
+#include "main.h"
+#include "main.h"
+#ifndef MAIN_H
+#include "main.h"
 
-	return (0);
-}

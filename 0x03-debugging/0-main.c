e2b4a1cd98413e766beffba49cfe6fa48c8a1627
Ayodeji Adefeso <ayodejiadefeso@gmail.com>
2023-02-23 16:06:22 -0800

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,75 +1,16 @@
-nclude <stdio.h>
-#include <stdio.h>
-#include <stdio.h>
-#include "main.h"
-/**
- *  * mqin -Test function for positive or negative
- *   *Reurn: 0
- *    */
-int main(void)
-{
-	        int i;
-#include "main.h"
-		/**
-		 *  * mqin -Test function for positive or negative
-		 *   *Reurn: 0
-		 *    */
-		int main(void)
-#include "main.h"
-			/**
-			 *  * mqin -Test function for positive or negative
-			 *   *Reurn: 0
-			 *    */
-			int main(void)
-			{
-#include "main.h"
-				/**
-				 *  * mqin -Test function for positive or negative
-				 *   *Reurn: 0
-				 *    */
-#include "main.h"
-				/**
-				 *  * mqin -Test function for positive or negative
-				 *   *Reurn: 0
-				 *    */
-#include "main.h"
-				/**
-				 *  * mqin -Test function for positive or negative
-				 *   *Reurn: 0
-				 *   #include "main.h"
-				 */**
-				 * mqin -Test function for positive or negative
-					 * #include "main.h"
-					 */**
-#include "main.h"
-					 /**
-					  *  * mqin -Test function for positive or negative
-					  *   *Reurn: 0
-					  *    */
-					 int main(void)
-					 {
-#include "main.h"
-						 /**
-						  *  * mqin -Test function for positive or negative
-						  *   *Reurn: 0
-						  *    */
-#include "main.h"
-						 /**
-						  *  * mqin -Test function for positive or negative
-						  *   *Reurn: 0
-						  *    */
-#include "main.h"
-						 /**
-						  *  * mqin -Test function for positive or negative
-						  *   *Reurn: 0
-						  *   #include "main.h"
-						  */**
-						  * mqin -Test function for positive or negative
-							  * #include "main.h"
-							  */**
-#include "main.h"
-#include "main.h"
-#include "main.h"
-#ifndef MAIN_H
 #include "main.h"
 
+/**
+ * main - tests functionthat prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}

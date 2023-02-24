9d31153abb84fa5b6d4e1be4ea38aec6f5dd20c3
Ayodeji Adefeso <ayodejiadefeso@gmail.com>
2023-02-24 15:47:44 -0800

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_most_numbers - Print numbers from 0 to 9
  * Descriptiom - print numbers excluding 2 and 4
- * Return: Always 0
+ * Return: The numbers since 0 up to 9
  */
 
 void print_most_numbers(void)

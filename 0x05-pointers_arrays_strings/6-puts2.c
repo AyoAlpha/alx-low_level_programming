1571032bfd72ede0193966fba46bc4ef7dc8706d
Ayodeji Adefeso <ayodejiadefeso@gmail.com>
2023-02-28 10:16:16 -0800

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - Entry point
+ * Description: Print the character of a string
+ * @str: string data type
+ * Return: str
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0);
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}

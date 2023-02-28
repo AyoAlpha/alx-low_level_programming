7b64dd85077715a744d3055061454c3eb0adada2
Ayodeji Adefeso <ayodejiadefeso@gmail.com>
2023-02-28 11:43:28 -0800

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - Entry point
+ * Description: Print the second half of the string
+ * @str - Data type
+ * Return: string
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int n, i;
+	char *p = str;
+
+	while (*p != '\0')
+	{
+		len++;
+		i++;
+	}
+	n = (len - 1) / 2 + 1;
+	for (i =n; i < len; i++)
+	{
+		_putchar(str[i]));
+	}
+	_putchar('\n');
+}

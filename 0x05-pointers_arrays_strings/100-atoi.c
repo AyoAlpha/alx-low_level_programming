310a0dc0f5e5e0c2a6793fa907f968e38918db3a
Ayodeji Adefeso <ayodejiadefeso@gmail.com>
2023-02-28 14:27:06 -0800

    Task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _atoi - Entry point
+ * Description: Convert string to integer
+ * @s: integer
+ * Return: char
+ */
+int _atoi(char *s)
+{
+        int sign = 1;
+	unsigned int ans = 0;
+
+	while (*s != '\0' && (*s < '0' || *s > '9'))
+	{
+		if (*s == '-')
+		{
+			sign *= -1;
+		}
+		s++;
+	}
+	while (*s >= '0' && *s <= '9')
+	{
+		ans = ans * 10 + (*s - '0');
+		s++;
+	}
+	return (sign * ans);
+}
+
+

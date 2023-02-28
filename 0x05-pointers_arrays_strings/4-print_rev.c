5b3ac6f7363e2de77fb87967c22aaeb347578a17
Ayodeji Adefeso <ayodejiadefeso@gmail.com>
2023-02-28 02:39:00 -0800

    Task 3

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - Entry point
+ * Description: function that prints a string, in reverse, followed by a new line.
+ * @s: integer
+ * Return: char
+ */
+void print_rev(char *s)
+{
+	int i =0;
+
+	while (s[i])
+	{
+		i++;
+	}
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}

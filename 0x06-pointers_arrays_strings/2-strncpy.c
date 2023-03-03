c9b33989f7d4f8b7f6e6cd8e96c12ed35f57020a
Ayodeji Adefeso <ayodejiadefeso@gmail.com>
2023-03-02 18:55:14 -0800

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncpy - Entry point
+ * Description: functions hat copies a string
+ * @src: the source string
+ * @dest: the destination string
+ * @n: Integer
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0, src_len = 0;
+
+	while (src[index++])
+	{
+		src_len++;
+	}
+	for (index = 0; src[index] && index < n ; index++)
+	{
+		dest[index] = src[index];
+	}
+	for (index = src_len; index < n; index++)
+	{
+		dest[index] = '\0';
+	}
+
+	return (dest);
+}

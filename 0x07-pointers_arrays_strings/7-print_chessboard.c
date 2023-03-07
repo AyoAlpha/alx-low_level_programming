a05f2e265e7edd226af80f6e6668e5f0106a4a34
Ayodeji Adefeso <ayodejiadefeso@gmail.com>
2023-03-07 08:22:29 -0800

    Task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Entry point
+ * Description:  function that prints the chessboard
+ * @a: array
+ * Return: Always 0 (success)
+ */
+void print_chessboard(char (*a)[8])
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 8; x++)
+	{
+		for (y = 0; y < 8; y++)
+			_putchar([x][y]);
+		_putchar('\n');
+	}
+}

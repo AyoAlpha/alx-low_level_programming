0880f6eec265832086de2a507704d35487d79d76
Ayodeji Adefeso <ayodejiadefeso@gmail.com>
2023-03-07 19:13:11 -0800

    Task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - Entry point
+ * Descriotion: function that sets value of a pointer
+ * @s: the double pointer
+ * @to: pointer
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

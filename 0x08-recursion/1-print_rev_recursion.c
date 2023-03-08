4e707fdf3c8f3aec8dfad36e1ec7a4f2c32e1f82
Ayodeji Adefeso <ayodejiadefeso@gmail.com>
2023-03-08 00:31:03 -0800

    Task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_rev_rcursion - Entry point
+ * Description: print a string in reverse
+ * @s: the string to reverse
+ * Return: always 0 (success)
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+}

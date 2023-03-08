84624f0dbb1c03c3e80d8722e4762f81103a95e9
Ayodeji Adefeso <ayodejiadefeso@gmail.com>
2023-03-08 13:00:09 -0800

    Task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * factorial - Entry point
+ * Description: returns a factorial
+ * @n: factorial
+ * Return: Always 0 (success)
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+		return (n * factorial(n-1));
+}

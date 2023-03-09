5fcd800a7615bcf15a32f07f250d4a2cb5aabdef
Ayodeji Adefeso <ayodejiadefeso@gmail.com>
2023-03-09 02:25:05 -0800

    Task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,7 +10,9 @@ int check_prime(int n, int i);
  */
 int is_prime_number(int n)
 {
-	return (check_prime(n, 1));
+	if (n <= 1)
+		return (0);
+	return (check_prime(n, n - 1));
 }
 
 /**
@@ -21,14 +23,9 @@ int is_prime_number(int n)
  */
 int check_prime(int n, int i)
 {
-	if (n <= 1)
-		return (0);
-
-	if (n % i == 0 && i > 1)
-		return (0);
-
-	if ((n / 1) < i)
+	if (i == 1)
 		return (1);
-
-	return (check_prime(n, i + 1));
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (check_prime(n, i - 1));
 }

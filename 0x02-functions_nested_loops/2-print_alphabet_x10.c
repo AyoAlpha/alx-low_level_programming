ccfe54a6fd27a0e0f7464b5cb08c7f99c0ca2a3b
Ayodeji Adefeso <ayodejiadefeso@gmail.com>
2023-05-23 05:29:55 -0700

    print all alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - printing all alphabet 10 times
+ *
+ * Return: on success
+ */
+
+void print_alphabet_x10(void)
+{
+	int x;
+	int y = 0;
+
+	while (y <= 10)
+	{
+		x = 'a';
+		while (x <= 'z')
+		{
+			putchar(x);
+			x++;
+		}
+		putchar('\n');
+		y++;
+	}
+}

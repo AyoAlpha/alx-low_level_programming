2dc6fc6a5064f4b9c48ac063c19fba0d8b38a365
Ayodeji Adefeso <ayodejiadefeso@gmail.com>
2023-05-24 14:44:20 -0700

    addition of two integer

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * print_last_digit - function to print last digit of any given integer
+ * @x: integer type
+ * Return: Always 0 on success
+ */
+
+int print_last_digt(int x)
+{
+	int print_last_digit = x;
+
+	if (x % 10)
+	{
+		return (print_last_digit);
+	}
+	return (0);
+}

b15c1155f387184f570992abab8439fa6fce8947
Ayodeji Adefeso <ayodejiadefeso@gmail.com>
2023-05-25 07:15:26 -0700

    print numbers ranging from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that print numbers ranging from 0 - 9
+ * 
+ * Return: Always 0 on success
+ */
+
+void print_numbers(void)
+{
+	int c = '0';
+
+	while (c <= '9')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}

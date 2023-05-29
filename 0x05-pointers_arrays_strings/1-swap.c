d5be337d22abee9ad3dac5f9bc6df5e1b82204aa
Ayodeji Adefeso <ayodejiadefeso@gmail.com>
2023-05-29 10:31:44 -0700

    swap two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - functions that swap value of two integers
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}

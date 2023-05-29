bf0d943927d2ec4b8f5e49a608e1cc5f0aeef371
Ayodeji Adefeso <ayodejiadefeso@gmail.com>
2023-05-29 10:22:09 -0700

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes an int as parameter and points it to 98
+ * @n: parameter
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

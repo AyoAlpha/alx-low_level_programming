40f00622a619aa623dc358e77c0e91d540d09f0a
Ayodeji Adefeso <ayodejiadefeso@gmail.com>
2023-06-13 01:57:31 -0700

    multiply arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,15 +11,20 @@
 int main(int argc, char *argv[])
 {
 	int i;
-	int mul = 0;
+	int mul = 1;
 
-	if (argc > 1)
+	if (argc > 2)
 	{
 		for (i = 1; i < argc; i++)
 		{
 			mul *= atoi(argv[i]);
 		}
 		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
 	}
 	return (0);
 }

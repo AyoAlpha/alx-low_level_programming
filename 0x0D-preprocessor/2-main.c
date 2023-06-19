fd19afe5a27f1e0d975053367b4c5d88742ccddc
Ayodeji Adefeso <ayodejiadefeso@gmail.com>
2023-06-19 10:01:18 -0700

    task 4

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#ifndef MACRO
+#define MACRO (2-main.c)
+#endif
+
+/**
+ * main - Entry point
+ *
+ * Return: void
+ */
+int main(void)
+{
+	printf("%s\n", MACRO);
+	return (0);
+}

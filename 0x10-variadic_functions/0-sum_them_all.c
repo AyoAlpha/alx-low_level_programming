34944d3e710ddf09bb685305c4de0bce1c0163e7
Ayodeji Adefeso <ayodejiadefeso@gmail.com>
2023-06-23 00:58:53 -0700

    task 1

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,12 +12,12 @@ int sum_them_all(const unsigned int n, ...)
 	va_list args;
 	unsigned int i, x = 0;
 
-	va_start (args, n);
+	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
 		x += va_arg(args, int);
 	}
-	va_end (args);
+	va_end(args);
 	return (x);
 }

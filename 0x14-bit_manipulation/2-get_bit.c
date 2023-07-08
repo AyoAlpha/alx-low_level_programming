9480c8a4f999a46a8ccf71474771f3f5e9378783
Ayodeji Adefeso <ayodejiadefeso@gmail.com>
2023-07-08 08:54:48 -0700

    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * getbit - function get the last bit
- * @n: nuber to search
- * @index:index of the bit
+ * get_bit - function get the last bit
+ * @n: number to search
+ * @index: index of the bit
  *
  * Return: 0 on succes and -1 on error
  */
